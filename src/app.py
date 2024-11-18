2da68502aa12fca0e64e28d5697166a494009bb2
Mikhail Khaykin <m.khaykin@outlook.com>
2024-11-18 18:58:56 +0300

    chore: pre-commit

--- src/app.py
+++ src/app.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 
-
 app = FastAPI()
+
 
 @app.get("/echo/{msg}")
 async def echo(msg: str) -> dict:
--- src/main.py
+++ src/main.py
@@ -1,10 +1,9 @@
 import uvicorn
 
-
-if __name__ == "__main__" :
+if __name__ == "__main__":
     uvicorn.run(
         app="app:app",
-        host="0.0.0.0",
+        host="0.0.0.0",  # noqa
         port=8000,
         reload=True,
     )
